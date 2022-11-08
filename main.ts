204d72b49d1a7c8952bdda321c5a7dac634f1589
alha501 <113909973+alha501@users.noreply.github.com>
2022-11-08 09:54:43 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,12 @@
 input.onButtonPressed(Button.A, function () {
-	
+    if (heroes > villains) {
+    	
+    } else if (false) {
+    	
+    }
 })
+let villains = 0
+let heroes: number[] = []
 let superheroes = [
 "Spider Man",
 "Captain Marvel",
@@ -8,12 +14,26 @@ let superheroes = [
 "Batman",
 "Thor"
 ]
+heroes = [
+6,
+5,
+4,
+7,
+8
+]
 let supervillains = [
 "Thanos",
 "Joker",
 "Hades",
 "Venom",
-"Magneto"
+"Dr Doom"
+]
+villains = [
+10,
+9,
+8,
+5,
+4
 ]
 basic.forever(function () {
 	
