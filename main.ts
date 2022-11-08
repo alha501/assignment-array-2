250242bcf7c9b15cd38975ef39c6385f398aaea7
alha501 <113909973+alha501@users.noreply.github.com>
2022-11-07 20:43:28 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,20 @@
+input.onButtonPressed(Button.A, function () {
+	
+})
+let superheroes = [
+"Spider Man",
+"Captain Marvel",
+"Captain America",
+"Batman",
+"Thor"
+]
+let supervillains = [
+"Thanos",
+"Joker",
+"Hades",
+"Venom",
+"Magneto"
+]
+basic.forever(function () {
+	
+})
